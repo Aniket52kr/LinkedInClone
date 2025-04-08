01469525625c7363d4a9936500d68347e0b83518
Aniket52kr <aniketbawankar2021@gmail.com>
2025-04-08 16:50:09 +0530

    debug code

--- Linkedin-clone-backend/controllers/authcontrollers.js
+++ Linkedin-clone-backend/controllers/authcontrollers.js
@@ -7,10 +7,9 @@ const { generateToken } = require("../utils/helpers");
 const { sendWelcomeEmail } = require("../emails/emailHandlers");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
-// Helper to determine if secure cookie should be used
 const isProduction = process.env.NODE_ENV === "production";
 
-// Registration Route
+// Register Route
 router.post("/register", async (req, res) => {
   const { firstName, lastName, userName, email, password } = req.body;
 
@@ -24,8 +23,7 @@ router.post("/register", async (req, res) => {
       return res.status(409).json({ message: "User already exists" });
     }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = await User.create({
       firstName,
@@ -36,12 +34,15 @@ router.post("/register", async (req, res) => {
     });
 
     const token = generateToken(newUser);
+
     res.cookie("token", token, {
       httpOnly: true,
-      maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
-      sameSite: "strict",
-      secure: isProduction,
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      sameSite: "None",
+      secure: true,
     });
+
+    console.log("Cookie set on registration:", token);
 
     res.status(201).json({
       message: "User registered successfully",
@@ -49,7 +50,6 @@ router.post("/register", async (req, res) => {
       token,
     });
 
-    // Send welcome email
     const profileUrl = `${process.env.CLIENT_URL}/profile/${newUser.firstName}`;
     try {
       await sendWelcomeEmail(
@@ -83,12 +83,15 @@ router.post("/login", async (req, res) => {
     bcrypt.compare(password, user.password, (err, result) => {
       if (result) {
         const token = generateToken(user);
+
         res.cookie("token", token, {
           httpOnly: true,
-          maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
-          sameSite: "strict",
-          secure: isProduction,
+          maxAge: 3 * 24 * 60 * 60 * 1000,
+          sameSite: "None",
+          secure: true,
         });
+
+        console.log("Cookie set on login:", token);
 
         res.status(200).json({
           message: "User logged in successfully",
@@ -109,15 +112,19 @@ router.post("/logout", (req, res) => {
   res.cookie("token", "", {
     httpOnly: true,
     expires: new Date(0),
-    sameSite: "strict",
-    secure: isProduction,
+    sameSite: "None",
+    secure: true,
   });
+
+  console.log("Cookie cleared on logout");
+
   res.status(200).json({ message: "User logged out successfully" });
 });
 
-// Me Route (Current User Info)
+// Me Route
 router.get("/me", isLoggedIn, async (req, res) => {
   try {
+    console.log("Cookies received in /me:", req.cookies);
     res.json(req.user);
   } catch (error) {
     console.error("Error in /me route:", error);
@@ -126,3 +133,155 @@ router.get("/me", isLoggedIn, async (req, res) => {
 });
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const router = express.Router();
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/user");
+// const { generateToken } = require("../utils/helpers");
+// const { sendWelcomeEmail } = require("../emails/emailHandlers");
+// const isLoggedIn = require("../middleware/isLoggedIn");
+
+// // Helper to determine if secure cookie should be used
+// const isProduction = process.env.NODE_ENV === "production";
+
+// // Registration Route
+// router.post("/register", async (req, res) => {
+//   const { firstName, lastName, userName, email, password } = req.body;
+
+//   if (!firstName || !userName || !email || !password) {
+//     return res.status(400).json({ message: "All fields are required" });
+//   }
+
+//   try {
+//     const existingUser = await User.findOne({ email });
+//     if (existingUser) {
+//       return res.status(409).json({ message: "User already exists" });
+//     }
+
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(password, salt);
+
+//     const newUser = await User.create({
+//       firstName,
+//       lastName,
+//       userName,
+//       email,
+//       password: hashedPassword,
+//     });
+
+//     const token = generateToken(newUser);
+//     res.cookie("token", token, {
+//       httpOnly: true,
+//       maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
+//       sameSite: "strict",
+//       secure: isProduction,
+//     });
+
+//     res.status(201).json({
+//       message: "User registered successfully",
+//       user: newUser,
+//       token,
+//     });
+
+//     // Send welcome email
+//     const profileUrl = ${process.env.CLIENT_URL}/profile/${newUser.firstName};
+//     try {
+//       await sendWelcomeEmail(
+//         newUser.email,
+//         newUser.firstName,
+//         newUser.lastName,
+//         profileUrl
+//       );
+//     } catch (emailErr) {
+//       console.error("Error sending welcome email:", emailErr);
+//     }
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// });
+
+// // Login Route
+// router.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+
+//   if (!email || !password) {
+//     return res.status(400).json({ message: "All fields are required" });
+//   }
+
+//   try {
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(404).json({ message: "User not found" });
+//     }
+
+//     bcrypt.compare(password, user.password, (err, result) => {
+//       if (result) {
+//         const token = generateToken(user);
+//         res.cookie("token", token, {
+//           httpOnly: true,
+//           maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
+//           sameSite: "strict",
+//           secure: isProduction,
+//         });
+
+//         res.status(200).json({
+//           message: "User logged in successfully",
+//           user,
+//           token,
+//         });
+//       } else {
+//         res.status(401).json({ message: "Invalid credentials" });
+//       }
+//     });
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// });
+
+// // Logout Route
+// router.post("/logout", (req, res) => {
+//   res.cookie("token", "", {
+//     httpOnly: true,
+//     expires: new Date(0),
+//     sameSite: "strict",
+//     secure: isProduction,
+//   });
+//   res.status(200).json({ message: "User logged out successfully" });
+// });
+
+// // Me Route (Current User Info)
+// router.get("/me", isLoggedIn, async (req, res) => {
+//   try {
+//     res.json(req.user);
+//   } catch (error) {
+//     console.error("Error in /me route:", error);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// });
+
+// module.exports = router;  
