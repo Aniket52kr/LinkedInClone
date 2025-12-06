a2ef9e05e03b8a2ae8323365247367f1ae9d8101
Aniket52kr <aniketbawankar2021@gmail.com>
2025-12-06 18:35:44 +0530

    fix path

--- Linkedin-clone-backend/routes/authRoutes.js
+++ Linkedin-clone-backend/routes/authRoutes.js
@@ -1,12 +1,14 @@
 // routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
+
 const { 
   register,
   login,
   logout,
   getCurrentUser,
-} = require('../controllers/authControllers'); 
+} = require("../controllers/authControllers"); 
+
 const isLoggedIn = require('../middleware/isLoggedIn');
 
 router.post('/register', register);
